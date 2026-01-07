017ee77ed2da759ff9f5e6f8fa5754a3f9be7eb6
lyfe00011 <lyfe00011@gmail.com>
2026-01-07 15:00:46 +0400

    fix

--- lib/Utils/sqlite-store.d.ts
+++ lib/Utils/sqlite-store.d.ts
@@ -1,14 +1,11 @@
 import { Sequelize } from 'sequelize';
-import type { AuthenticationCreds, SignalKeyStore } from '../Types/index.js';
+import { AuthenticationState } from '../../src/Types';
 /**
- * A Sequelize-based SQLite store for Baileys.
- * This store persists both credentials and cryptographic keys in a single SQLite database.
+ * A Sequelize-based auth state store for Baileys.
+ * This store persists both credentials and cryptographic keys.
  */
-export declare const makeSequelizeAuthState: (sequelize: Sequelize) => Promise<{
-    state: {
-        creds: AuthenticationCreds;
-        keys: SignalKeyStore;
-    };
+export declare const makeSequelizeAuthState: (sequelize: Sequelize, logger?: any) => Promise<{
+    state: AuthenticationState;
     saveCreds: () => Promise<void>;
 }>;
 //# sourceMappingURL=sqlite-store.d.ts.map
